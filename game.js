b696e2595d2579dcc8f71f38d2f43795f39511f9
MajdHarbb <majdwalidharb@gmail.com>
2022-01-23 18:42:49 +0200

    green background

--- game.js
+++ game.js
@@ -10,6 +10,7 @@ window.onload = function () {
 
 
     // if cursor is over a wall --> change maze background (class youlose)
+    
     for (var i = 0; i < walls.length - 1; i++) {
         
         walls[i].addEventListener("mouseover", YouLost,false);
@@ -22,19 +23,24 @@ window.onload = function () {
     function YouWon(){
         
         score = score +10;
-        document.getElementById("status").innerHTML = "You Won! :) +10 points ";
+        document.getElementById("status").innerHTML = "You Won! :) +5 points ";
         document.getElementsByClassName("example")[0].innerHTML = score;
+        for(var i = 0; i < walls.length-1; i++){
+            walls[i].className += " youwon";
+        }
 
     }
     //end function YouWon()
     
     // --> append "youlose" class, -10 score
     function YouLost() {
+
+
         var local_score = 0;
         for (var i = 0; i < walls.length - 1; i++) {
             walls[i].className += " youlose";            
         }
-        document.getElementById("status").innerHTML = "You Lost :(";
+        document.getElementById("status").innerHTML = "You Lost :( -10 points";
         local_score = score - 10 ;
         score = local_score;
         document.getElementsByClassName("example")[0].innerHTML = score;
@@ -47,7 +53,9 @@ window.onload = function () {
 
         for (var i = 0; i < walls.length - 1; i++) {
             walls[i].classList.remove("youlose");
+            walls[i].classList.remove("youwon");
         }
+        document.getElementById("status").innerHTML = "New Round";
 
 
     }
